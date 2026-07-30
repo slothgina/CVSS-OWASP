66111fb90c889c4b4f053006aa151c60d21f2f4e
slothgina <gina.slothstudio@zohomail.com>
2026-07-30 16:54:18 -0400

    Added relay command.

---
+++ SlothSecDesktopApp/RelayCommand.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Input;
+
+namespace SlothSecDesktopApp;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object?> _execute;
+
+    public RelayCommand(Action<object?> execute)
+    {
+        _execute = execute;
+    }
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter) => _execute(parameter);
+
+    public event EventHandler? CanExecuteChanged;
+}
